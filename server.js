c08aae01c36be76efd047d583e59137559151ff9
Hexx-cyber <w281133@gmail.com>
2026-06-09 15:36:49 +0500

    server.js

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ const axios = require('axios');
 const app = express();
 
 app.all('*', async (req, res) => {
+    const TARGET = "https://ff.garena.com"; 
+    
     try {
-        // Garena ka real server URL
-        const TARGET = "https://ff.garena.com"; 
-        
         const response = await axios({
             method: req.method,
             url: `${TARGET}${req.url}`,
             headers: {
                 ...req.headers,
-                'Host': 'ff.garena.com' // Ye header bahut important hai
+                'Host': 'ff.garena.com',
+                // Sabse zaroori: Android phone ka disguise
+                'User-Agent': 'FreeFire/1.123.17 Android/11',
+                'Referer': 'https://ff.garena.com/'
             },
             data: req.method !== 'GET' ? req.body : undefined,
             validateStatus: () => true 
         });
 
-        res.status(response.status).set(response.headers).send(response.data);
+        // Kuch headers ko modify karna padta hai taaki client crash na ho
+        res.status(response.status).set(response.headers).removeHeader('content-encoding').send(response.data);
     } catch (err) {
         res.status(502).send("Proxy error: " + err.message);
     }
