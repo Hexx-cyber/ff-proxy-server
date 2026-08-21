d70a6a7e2a5903594539dffd6bca4388cd2928da
Hexx-cyber <w281133@gmail.com>
2026-08-22 00:04:05 +0500

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const admin = require('firebase-admin');
+const path = require('path');
 
 // Vercel Environment Variable se key ko read karna
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
@@ -20,5 +21,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ status: "Powerful Firebase Server is Live!" });
 });
-
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, 'favicon.ico'));
+});
 module.exports = app;
